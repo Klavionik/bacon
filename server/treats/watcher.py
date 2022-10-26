e8c4d4014cf458c07044232fa9f96467fc4ae849
Roman Vlasenko <klavionik@gmail.com>
2022-10-26 11:58:16 +0300

    Install huey and create periodic task to update Perekrestok prices

--- server/treats/watcher.py
+++ server/treats/watcher.py
@@ -1,6 +1,16 @@
 import asyncio
-
+from huey import FileHuey, MemoryHuey, crontab
 from watcher.tasks import update_perekrestok_products
+from config import settings
 
-if __name__ == '__main__':
+if settings.DEBUG:
+    huey = MemoryHuey()
+    schedule = crontab()
+else:
+    huey = FileHuey('/var/lib/huey/data')
+    schedule = crontab(minute='0', hour='*/1')
+
+
+@huey.periodic_task(schedule)
+def run_update_perekrestok_prices():
     asyncio.run(update_perekrestok_products())
