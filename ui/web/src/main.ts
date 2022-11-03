93dcb20d5dd6721dd46c0e6ec8edfbc2ceb4e40c
Roman Vlasenko <klavionik@gmail.com>
2022-11-04 01:09:33 +0300

    Setup toasts

--- ui/web/src/main.ts
+++ ui/web/src/main.ts
@@ -1,16 +1,23 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 import { Vue3ProgressPlugin } from "@marcoschulte/vue3-progress"
-
+import Toast from "vue-toastification"
 import App from "./App.vue"
 import router from "@/router/index"
 
 import "vue-select/dist/vue-select.css"
 import "@marcoschulte/vue3-progress/dist/index.css"
+import "vue-toastification/dist/index.css"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Vue3ProgressPlugin)
+app.use(Toast, {
+  transition: "Vue-Toastification__fade",
+  maxToasts: 4,
+  draggable: false,
+  hideProgressBar: true,
+})
 app.mount("#app")
