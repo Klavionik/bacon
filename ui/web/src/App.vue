e574351a07b289186dbd1922f4f0a3573402e745
Roman Vlasenko <klavionik@gmail.com>
2022-11-06 18:37:52 +0300

    Handle Unauthorized error inside components

--- ui/web/src/App.vue
+++ ui/web/src/App.vue
@@ -9,10 +9,26 @@
 import { RouterView } from "vue-router"
 import { defineComponent } from "vue"
 import { ProgressBar } from "@marcoschulte/vue3-progress"
+import { Unauthorized } from "@/http/errors"
+import { useToast } from "vue-toastification"
+import { useUserStore } from "@/stores/user"
 
 export default defineComponent({
   name: "App",
   components: { RouterView, ProgressBar },
+  setup() {
+    const { logout } = useUserStore()
+    const toast = useToast()
+    return { logout, toast }
+  },
+  errorCaptured(err) {
+    if (err instanceof Unauthorized) {
+      this.toast.warning("Время сессии истекло. Войдите заново.")
+      this.logout()
+      this.$router.push("/")
+      return false
+    }
+  },
 })
 </script>
 
