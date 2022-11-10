bee9e106ca1c15513dc63cd94de7470bf4d49d2e
Roman Vlasenko <klavionik@gmail.com>
2022-11-10 21:11:24 +0300

    Update shell

--- server/treats/shell.py
+++ server/treats/shell.py
@@ -1,27 +1,19 @@
-from storage import create_db_engine, create_db_session, models
+from storage import create_db_engine, create_db_session
+from storage.models import *  # noqa
 from config import settings
-from datetime import datetime as dt
+from ptpython import embed
 
 engine = create_db_engine(settings.db_uri, echo=True)
 Session = create_db_session(engine)
 
-# roma_data = dict(username='jediroman', language='ru')
-# perekrestok_data = dict(title='Перекресток', url_rule=r'^https://(w{3}\.)?perekrestok\.ru/cat/.+$')
-# perekrestok_surgut_data = dict(location_id=316, name='Сургут Агора', address='г. Сургут, ул. Профсоюзов, д. 11')
-# curd_snack = dict(
-#     title='Сырок Б.Ю. Александров',
-#     url='https://www.perekrestok.ru/cat/373/p/syrok-tvoroznyj-rostagroeksport-s-aromatom-vanili-glazirovannyj-20-45g-3666242'
-# )
-# curd_snack_price = dict(price=420.0, created_at=dt(2022, 9, 8, 16))
-# curd_snack_price_newer = dict(price=600, created_at=dt(2022, 9, 20, 15))
-#
-#
-# async def create_test_rows():
-#     async with Session() as session, session.begin():
-#         roma = models.User(**roma_data)
-#         perekrestok = models.Shop(**perekrestok_data)
-#         perekrestok_surgut = models.ShopLocation(**perekrestok_surgut_data, shop=perekrestok)
-#         session.add_all([roma, perekrestok, perekrestok_surgut])
-#         await session.commit()
-#
-#     return roma
+
+def configure(repl):
+    repl.show_signature = True
+    repl.use_code_colorscheme('monokai')
+    repl.color_depth = 'DEPTH_24_BIT'
+    repl.insert_blank_line_after_output = False
+    repl.complete_while_typing = False
+    repl.confirm_exit = False
+
+
+embed(globals(), locals(), configure)
