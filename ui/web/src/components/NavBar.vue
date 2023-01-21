13bf6653867b497421607f7c864e03194e1fc4f1
Roman Vlasenko <klavionik@gmail.com>
2023-01-21 19:21:26 +0300

    Wrap navbar with container and change the logo

--- ui/web/src/components/NavBar.vue
+++ ui/web/src/components/NavBar.vue
@@ -1,44 +1,46 @@
 <template>
   <nav class="navbar is-light is-spaced">
-    <div class="navbar-brand">
-      <RouterLink class="navbar-item" to="/">
-        <img src="/logo.jpg" alt="Treats" />
-      </RouterLink>
-      <a
-        class="navbar-burger"
-        :class="{ 'is-active': isHamburgerOpen }"
-        @click="isHamburgerOpen = !isHamburgerOpen"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
-    </div>
-    <div class="navbar-menu" :class="{ 'is-active': isHamburgerOpen }">
-      <div class="navbar-start">
-        <RouterLink v-if="loggedIn" class="navbar-item" :to="{ name: 'treats' }">
-          Мои вкусняшки
-        </RouterLink>
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img class="treats-logo" src="@/assets/logo.svg" alt="Treats" />
+        </a>
+        <a
+          class="navbar-burger"
+          :class="{ 'is-active': isHamburgerOpen }"
+          @click="isHamburgerOpen = !isHamburgerOpen"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
       </div>
-      <div class="navbar-end">
-        <div v-if="!loggedIn" class="navbar-item">
-          <RouterLink is="button" class="button" :to="{ name: 'login' }">Войти</RouterLink>
+      <div class="navbar-menu" :class="{ 'is-active': isHamburgerOpen }">
+        <div class="navbar-start">
+          <RouterLink v-if="loggedIn" class="navbar-item" :to="{ name: 'treats' }">
+            Мои вкусняшки
+          </RouterLink>
         </div>
-        <template v-else-if="loggedIn && !isHamburgerOpen">
-          <div class="navbar-item mx-3">{{ email }}</div>
-          <div class="navbar-item buttons">
-            <RouterLink class="button is-light" :to="{ name: 'profile' }">
-              <i class="fa-sharp fa-solid fa-gear fa-lg" />
-            </RouterLink>
-            <a class="button is-light" @click="logout">
-              <i class="fa-sharp fa-solid fa-arrow-right-from-bracket fa-lg" />
-            </a>
+        <div class="navbar-end">
+          <div v-if="!loggedIn" class="navbar-item">
+            <RouterLink is="button" class="button" :to="{ name: 'login' }">Войти</RouterLink>
           </div>
-        </template>
-        <template v-else>
-          <RouterLink class="navbar-item" :to="{ name: 'profile' }">Профиль</RouterLink>
-          <a class="navbar-item" href="#" @click="logout">Выйти</a>
-        </template>
+          <template v-else-if="loggedIn && !isHamburgerOpen">
+            <div class="navbar-item mx-3">{{ email }}</div>
+            <div class="navbar-item buttons">
+              <RouterLink class="button is-light" :to="{ name: 'profile' }">
+                <i class="fa-sharp fa-solid fa-gear fa-lg" />
+              </RouterLink>
+              <a class="button is-light" @click="logout">
+                <i class="fa-sharp fa-solid fa-arrow-right-from-bracket fa-lg" />
+              </a>
+            </div>
+          </template>
+          <template v-else>
+            <RouterLink class="navbar-item" :to="{ name: 'profile' }">Профиль</RouterLink>
+            <a class="navbar-item" href="#" @click="logout">Выйти</a>
+          </template>
+        </div>
       </div>
     </div>
   </nav>
@@ -73,3 +75,13 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+.treats-logo {
+  height: 30px;
+}
+
+.navbar-brand .navbar-item:hover {
+  background-color: transparent !important;
+}
+</style>
