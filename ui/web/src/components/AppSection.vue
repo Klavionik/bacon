1c572d3363c571043411942c658f8c27ec7af813
Roman Vlasenko <klavionik@gmail.com>
2023-01-23 00:40:04 +0300

    Add VueUse library

---
+++ ui/web/src/components/AppSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section" :class="">
+    <div class="container"></div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "Section",
+})
+</script>
+
+<style scoped></style>
