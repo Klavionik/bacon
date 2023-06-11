91605acff7488e98995a5ae48d0fdd339d2164e6
Roman Vlasenko <klavionik@gmail.com>
2023-06-11 22:49:06 +0300

    Products app models

---
+++ backend/web/products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from products import models
+
+admin.site.register(models.Retailer)
+admin.site.register(models.Store)
+admin.site.register(models.Product)
+admin.site.register(models.UserProduct)
+admin.site.register(models.UserStore)
+
+
+@admin.register(models.Price)
+class PriceAdmin(admin.ModelAdmin):
+    readonly_fields = ["created_at"]
---
+++ backend/web/products/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Retailer(models.Model):
+    title = models.CharField(max_length=64)
+    display_title = models.CharField(max_length=64)
+
+
+class Store(models.Model):
+    title = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    external_id = models.CharField(max_length=128)
+    retailer = models.ForeignKey(Retailer, related_name="stores", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint("retailer", "external_id", name="uniq_retailer")]
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.URLField()
+    available = models.BooleanField(default=True)
+    meta = models.JSONField(default=dict, blank=True)
+    store = models.ForeignKey(Store, related_name="products", on_delete=models.RESTRICT)
+
+
+class Price(models.Model):
+    value = models.FloatField()
+    product = models.ForeignKey(Product, related_name="prices", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserProduct(models.Model):
+    user = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="users", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint("user", "product", name="uniq_user_product")]
+
+
+class UserStore(models.Model):
+    user = models.ForeignKey(User, related_name="stores", on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, related_name="+", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint("user", "store", name="uniq_user_store")]
