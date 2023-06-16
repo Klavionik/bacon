fbe8190c8d9f4555567aebb00896ccb9b6afd848
Roman Vlasenko <klavionik@gmail.com>
2023-06-17 02:04:28 +0300

    Use JSONField for scraper config

--- backend/web/scraping/models.py
+++ backend/web/scraping/models.py
@@ -1,7 +1,4 @@
-import tomli
-import tomli_w
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.module_loading import import_string
 
@@ -13,29 +10,9 @@ def get_scrapers_choices():
     return list(zip(scrapers, scrapers))
 
 
-class TOMLField(models.TextField):
-    description = "A TOML-compliant text field."
-
-    def get_prep_value(self, value):
-        return tomli_w.dumps(value)
-
-    def value_from_object(self, obj):
-        value = super().value_from_object(obj)
-        return tomli_w.dumps(value)
-
-    def from_db_value(self, value, expression, connection):
-        if value is None:
-            return value
-
-        try:
-            return tomli.loads(value)
-        except tomli.TOMLDecodeError:
-            raise ValidationError("Value must be valid TOML.")
-
-
 class Scraper(models.Model):
     entrypoint = models.CharField(max_length=256, choices=get_scrapers_choices())
-    config = TOMLField(blank=True)
+    config = models.JSONField(blank=True, default=dict)
 
     @property
     def instance(self):
