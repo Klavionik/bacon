a25f0e6c2839ded67f2f8cd7f634b526dd8694cc
Roman Vlasenko <klavionik@gmail.com>
2023-06-23 02:03:18 +0300

    Extend user token lifetime to 7 days

--- backend/web/settings.py
+++ backend/web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 import environ
@@ -148,6 +148,7 @@ DJOSER = {
 # djangorestframwork-simplejwt settings.
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ["JWT"],
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 
