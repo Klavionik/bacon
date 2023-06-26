2ed315f9ca5b4071566873cafb1b70d1bc84eadd
Roman Vlasenko <klavionik@gmail.com>
2023-06-27 02:28:29 +0300

    Rewrite Dockerfile and docker-compose.yml

--- backend/web/api/apps.py
+++ backend/web/api/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "web.api"
-
-    def ready(self):
-        super().ready()
-
-        from web.api import receivers  # noqa
--- backend/web/products/apps.py
+++ backend/web/products/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ProductsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "web.products"
+
+    def ready(self):
+        super().ready()
+
+        from web.products import receivers  # noqa
