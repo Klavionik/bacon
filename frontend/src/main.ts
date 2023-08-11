a7ae3542cc72f86cebf2f98a669f665c3c4dd3ec
Roman Vlasenko <klavionik@gmail.com>
2023-08-11 12:17:31 +0300

    Display app version on startup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,8 +9,9 @@ import "vue-select/dist/vue-select.css"
 import "@marcoschulte/vue3-progress/dist/index.css"
 import "vue-toastification/dist/index.css"
 
-const app = createApp(App)
+console.info("App version", import.meta.env.VITE_APP_VERSION)
 
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(Vue3ProgressPlugin)
@@ -20,4 +21,5 @@ app.use(Toast, {
   draggable: false,
   hideProgressBar: true,
 })
+
 app.mount("#app")
